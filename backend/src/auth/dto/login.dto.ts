8e03232fbb3d0e0ce47a7db5440651fabe73eed5
brunoramossilva <brunogabriel0900@gmail.com>
2026-04-21 19:15:27 -0300

    refactor: remove eslint-disable comments from DTOs

--- backend/src/auth/dto/login.dto.ts
+++ backend/src/auth/dto/login.dto.ts
@@ -2,13 +2,10 @@ import { IsEmail, IsString, MinLength } from 'class-validator';
 
 export class LoginDto {
   // class-validator decorators are runtime metadata factories.
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   @IsEmail()
   email!: string;
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   @IsString()
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   @MinLength(6)
   password!: string;
 }
